828491ef8be7e32bd9e7cfc75de2f19338f996bf
Denis Zalevskiy <denis.zalevskiy@jolla.com>
2014-06-16 15:47:11 +0300

    testing snapshot-revert and corresponding changes
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jolla.com>

--- tests/test_vault.sh
+++ tests/test_vault.sh
@@ -12,15 +12,20 @@ src=`cd $cdir/../src;pwd`
 
 source $src/the-vault_misc $src
 if [ "x$vault_misc" == "x" ]; then
-    echo "vault untils are not imported"
+    echo "vault utils are not imported"
     exit 1
 fi
 
 echo "TEST DIR: $vault"
-rmdir $vault || error 899 "$vault is not empty? Need empty dir"
+rmdir $vault || error 99 "$vault is not empty? Need empty dir"
+
+function on_exit {
+    echo "REMOVE: $vault"
+}
+trap on_exit EXIT
 
 function test_error {
-    error $(expr + $1) "$0: ERROR in line $1"
+    error 222 "LINE $1"
 }
 
 fname=
@@ -29,21 +34,27 @@ prefix=
 module=
 data=
 
+function gen_data {
+    local module=$1 count=$2
+    echo "DATA:$module:$count"
+}
+
 function new_fname {
     prefix=$(printf "%03d" $count)
     fname="file$prefix"
-    data="DATA:$module:$count"
+    data=$(gen_data $module $count)
     echo "New fname $fname"
     count=$(expr $count + 1)
 }
 
 function add_file_commit {
-    fname=$1
-    data=$2
-    unit=$3
-    dst=$unit/$fname
-    echo "$data" > $dst || error 804 "Can't write to $dst" 
-    the-vault-submodule-commit $unit "$data" || error 805 "Can't commit $unit"
+    local fname=$1 data=$2 unit=$3
+    local dst=$unit/$fname
+    echo "Write $data to the `pwd`/$dst"
+    cat $dst
+    echo "$data" > $dst || error 84 "Can't write to $dst" 
+    cat $dst
+    the-vault-submodule-commit $unit "$data" || error 85 "Can't commit $unit"
 }
 
 function check_snapshots {
@@ -53,20 +64,56 @@ function check_snapshots {
     fi
 }
 
+snapshots_count=0
+function new_snapshot {
+    echo "New snapshot for $module"
+    the-vault-snapshot-commit "$data" "$data in $module" || test_error $1
+    snapshots_count=$(expr $snapshots_count + 1)
+    check_snapshots $snapshots_count
+}
+
+function check_file {
+    local name=$1
+    local data=$2
+}
+
 the-vault-create $vault user user@test || error 901 "Can't create"
 cd $vault || error 902 "Can't enter $vault"
 
 module=Gallery
-the-vault-submodule-add Gallery || error 903
+the-vault-submodule-add $module || error 93
 
 new_fname
 add_file_commit $fname "$data" "$module"
-the-vault-snapshot-commit "$data" "$data in $module" || test_error $LINENO
-check_snapshots 1
+new_snapshot $LINENO
+snapshot1=$(the-vault-snapshot-list | head -n 1)
 
 new_fname
 add_file_commit $fname "$data" "$module"
-the-vault-snapshot-commit "$data" "$data in $module" || test_error $LINENO
-check_snapshots 2
+new_snapshot $LINENO
 
+module=People
+the-vault-submodule-add $module 0 || error 93
+
+new_fname
+add_file_commit $fname "$data" "$module"
+new_snapshot $LINENO
+
+new_fname
+add_file_commit $fname "$data" "$module"
+new_snapshot $LINENO
+
+module=Gallery
+new_fname
+add_file_commit $fname "$data" "$module"
+new_snapshot $LINENO
+
+add_file_commit file000 "$data" "$module"
+new_snapshot $LINENO
+
+snapshotN=$(the-vault-snapshot-list | tail -n 1)
+
+the-vault-snapshot-revert $snapshot1 || error 43 "Can't revert to $snapshot1"
+
+the-vault-snapshot-revert $snapshotN || error 43 "Can't revert to $snapshot1"
 echo "DONE"
